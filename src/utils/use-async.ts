98bc1003ac655ed5352aaf255802da87a1125a96
guofeiyua <1934264618@qq.com>
2024-03-15 15:44:13 +0800

    chore: 用useReducer改写useAsync

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,5 +1,5 @@
 import { useMountedRef } from "./index";
-import { useCallback, useState } from "react";
+import { useCallback, useReducer, useState } from "react";
 interface State<D> {
   data: D | null;
   error: Error | null;
@@ -10,34 +10,48 @@ const defaultInitialState: State<null> = {
   data: null,
   stat: "idle",
 };
+
+const useSafeDispatch = <T>(dispatch: (...args: T[]) => void) => {
+  const mountedRef = useMountedRef();
+  return useCallback(
+    (...args: T[]) => (mountedRef.current ? dispatch(...args) : void 0),
+    [dispatch, mountedRef]
+  );
+};
 export const useAsync = <D>(initState?: State<D>) => {
-  const [state, setState] = useState<State<D>>({
-    ...defaultInitialState,
-    ...initState,
-  });
+  const [state, dispatch] = useReducer(
+    (state: State<D>, action: Partial<State<D>>) => ({
+      ...state,
+      ...action,
+    }),
+    {
+      ...defaultInitialState,
+      ...initState,
+    }
+  );
+  const safeDispatch = useSafeDispatch(dispatch);
   const setData = useCallback(
     (data: D) =>
-      setState({
+      safeDispatch({
         data,
         error: null,
         stat: "success",
       }),
-    []
+    [safeDispatch]
   );
   const setError = useCallback(
     (error: Error) =>
-      setState({
+      safeDispatch({
         error,
         data: null,
         stat: "error",
       }),
-    []
+    [safeDispatch]
   );
 
   // retry保存下上次的网络请求，方便其他地方调用
   const [retry, setRetry] = useState(() => () => {});
-  // 查看组件是否已被卸载，防止在卸载的组件赋值
-  const mountedRef = useMountedRef();
+
   const run = useCallback(
     (promise: Promise<D>, config?: { retryFun: () => Promise<D> }) => {
       if (!promise || !promise.then) {
@@ -49,28 +63,21 @@ export const useAsync = <D>(initState?: State<D>) => {
         }
       });
 
-      // setState的函数式赋值
-      setState((prevState) => ({
-        ...prevState,
+      safeDispatch({
         stat: "loading",
-      }));
+      });
 
       return promise
         .then((data) => {
-          console.log(mountedRef.current, "mountedRef.current");
-          if (mountedRef.current) {
-            setData(data);
-          }
+          setData(data);
           return data;
         })
         .catch((error) => {
-          if (mountedRef.current) {
-            setError(error);
-          }
+          setError(error);
           return error;
         });
     },
-    [setData, setError, mountedRef]
+    [setData, setError, safeDispatch]
   );
   return {
     isIdle: state.stat === "idle",
